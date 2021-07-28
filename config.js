e74adfb15663e197fe26b0ae07cec9215cb0b767
ttruccc <88078472+ttruccc@users.noreply.github.com>
2021-07-28 14:23:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'Tui biết mà 🥰. Yêu nhiều nhiều 😘😘',
     messDesc: 'I love youuuu.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/thhttrucc
+    messLink: 'https://www.facebook.com/thhttrucc'
 }
